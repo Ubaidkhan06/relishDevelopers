59a220593349069190f34eac0b00e38628500c50
Ubaidkhan <ubaidkhan0631@gmail.com>
2023-08-04 18:58:50 +0530

    minor issue

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,14 +4,20 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { prisma } from "@/lib/prisma";
 
-const Blog = async () => {
+const Blog = async ({ searchParams }) => {
+  
+  // !getting id from search params
+  const id = searchParams?.id || null;
+
   // !get all the values from the database
   const data = await prisma.blogs.findMany();
+  
 
   return (
     <Navbar>
       <div className="flex flex-col w-full">
         <div className="body flex flex-col items-center">
+
           <div className="bg-base-100 text-base-content p-6 w-screen flex flex-col items-center mt-24">
             <h1 className="text-center text-3xl lg:text-5xl my-2">Our Blog</h1>
 
