6deafec678ad3d1ef886cc9216c501d317d3826e
Ubaidkhan <ubaidkhan0631@gmail.com>
2023-08-04 19:54:17 +0530

    changed spreadhseet credentials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <p>
           Relish Development Solutions
           <br />
-          Providing reliable tech since 1992
+          Providing reliable tech since 2018
         </p>
       </div>
       <div className="">
@@ -33,7 +33,58 @@ const Footer = () => {
       </div>
       <div>
         <span className="footer-title">Legal</span>
-        <Link href={"/privacypolicy"} className="link link-hover">Privacy policy</Link>
+        <Link href={"/privacypolicy"} className="link link-hover">
+          Privacy policy
+        </Link>
+      </div>
+      <div>
+        <span className="footer-title">Contact</span>
+        <div>
+          <Link
+            className="text-base"
+            href={"mailto:sales@relishdevelopers.com"}
+          >
+            relishdevelopers@gmail.com
+          </Link>
+        </div>
+        <Link className="text-base" href={"tel:+91 7565981836"}>
+          +91 75659 81836
+        </Link>
+        <div className="text-base">Greater Noida, IN</div>
+        <div className="flex justify-center items-center gap-3">
+          <Link
+            target={"_blank"}
+            href={"https://www.linkedin.com/company/relishdevelopers/"}
+          >
+            <Image
+              src={"/images/lin.svg"}
+              width={50}
+              height={50}
+              alt="linkedin"
+            />
+          </Link>
+          <Link
+            target={"_blank"}
+            href={
+              "https://instagram.com/official_relishdevelopment?igshid=MzRlODBiNWFlZA=="
+            }
+          >
+            <Image
+              src={"/images/insta1.svg"}
+              width={50}
+              height={50}
+              alt="linkedin"
+            />
+          </Link>
+          <Link href={"https://www.facebook.com"} target="_blank">
+            <Image
+              src={"/images/fb1.svg"}
+              width={40}
+              height={40}
+              alt="linkedin"
+            />
+          </Link>
+        </div>
       </div>
     </footer>
   );
